0a465d23f40d16c3323b5d2cbedaf105e716045c
Jyotman Singh <jyotman94@gmail.com>
2018-07-18 00:39:38 +0800

    GET user tweets API

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -4,9 +4,18 @@ const Tweet = require('../model/Tweet');
 
 exports.createTweet = async (req, res, next) => {
     try {
-        const tweet = new Tweet({handle: req.headers.user, body: req.body.tweet});
+        const tweet = new Tweet({handle: req.ip, body: req.body.tweet});
         await tweet.save();
         res.end();
+    } catch (err) {
+        next(err);
+    }
+}
+
+exports.getTweetsForUser = async (req, res, next) => {
+    try {
+        const tweets = await Tweet.find({handle: req.ip});
+        res.send(tweets);
     } catch (err) {
         next(err);
     }
--- server/route/tweet.js
+++ server/route/tweet.js
@@ -5,4 +5,6 @@ const router = require('express').Router();
 
 router.post('/', tweetController.createTweet);
 
+router.get('/', tweetController.getTweetsForUser);
+
 module.exports = router
