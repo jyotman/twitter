b95a4c172fae24b643c6fc2099618f628a5a4512
Jyotman Singh <jyotman94@gmail.com>
2018-07-18 23:42:19 +0800

    Adjust App.js to use Twitter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Tweets from "./components/Tweets";
+import Twitter from "./components/Twitter";
 
 class App extends Component {
   render() {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Twitter Clone</h1>
         </header>
-		<Tweets/>
+        <Twitter />
       </div>
     );
   }
